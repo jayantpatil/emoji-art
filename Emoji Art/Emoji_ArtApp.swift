ac25f5ef0579363e4125c89905d09069424d49e7
Jayant D. Patil <jayant.ptl@gmail.com>
2024-10-06 23:17:18 +0530

    Initial Commit

---
+++ Emoji Art/Emoji_ArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_ArtApp.swift
+//  Emoji Art
+//
+//  Created by Jayant D Patil on 06/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_ArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
